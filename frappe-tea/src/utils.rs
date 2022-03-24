a34d2cca26640aff3613792808d4f4a4490fdeca
Jose Quesada <jquesada2016@fau.edu>
2022-03-24 12:16:20 -0500

    [feat] added thread pool impl to  for non-wasm targets

--- frappe-tea/src/utils.rs
+++ frappe-tea/src/utils.rs
@@ -1,7 +1,8 @@
 use core::future::Future;
 #[cfg(not(target_arch = "wasm32"))]
-use futures::executor::block_on;
-use std::ops::Deref;
+use futures::executor::ThreadPool;
+use futures::task::SpawnExt;
+use std::{lazy::SyncOnceCell, ops::Deref, sync::Arc};
 #[cfg(target_arch = "wasm32")]
 use wasm_bindgen_futures::spawn_local;
 
@@ -12,12 +13,20 @@ macro_rules! api_planning {
     ($($tt:tt)*) => {};
 }
 
-pub fn execute_async<Fut>(future: Fut)
+pub async fn execute_async<Fut>(future: Fut)
 where
-    Fut: Future<Output = ()> + 'static,
+    Fut: Future<Output = ()> + Send + 'static,
 {
     #[cfg(not(target_arch = "wasm32"))]
-    block_on(future);
+    {
+        static THREAD_POOL: SyncOnceCell<ThreadPool> = SyncOnceCell::new();
+
+        let tp = THREAD_POOL.get_or_init(|| {
+            ThreadPool::new().expect("failed to create thread pool")
+        });
+
+        tp.spawn(future);
+    }
 
     #[cfg(target_arch = "wasm32")]
     spawn_local(future);
